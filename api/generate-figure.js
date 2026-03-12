e046ed9d03f51f20dad6f0b912458a026b672970
clivia-2026 <15622173450@163.com>
2026-03-12 19:12:55 +0800

    Update generate-figure.js

--- api/generate-figure.js
+++ api/generate-figure.js
@@ -18,7 +18,7 @@ export default async function handler(req, res) {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
-        model: 'glm-4v-flash',
+        model: 'glm-4v',
         max_tokens: 4096,
         messages: [{
           role: 'user',
